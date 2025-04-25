818d9aab2f2ce9f718ce9c598c7ecf4c3496490c
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-04-25 20:26:33 +0600

    faq section is done here

---
+++ script.js
@@ -0,0 +1,10 @@
+// faq section is here 
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const btn = item.querySelector('.faq-question');
+  btn.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
